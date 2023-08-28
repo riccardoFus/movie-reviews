e158ca7085448f8b6f6041a276b1d3b82ce6b47c
RiccardoFusiello aka Cavi <93785795+riccardoFus@users.noreply.github.com>
2023-08-28 16:39:11 +0200

    modifica frontend

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function Login() {
-  return <div className="App">Login</div>;
-}
+const Login = (props) => {
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+
+  const onChangeName = (e) => {
+    const name = e.target.value;
+    setName(name);
+  };
+
+  const onChangeId = (e) => {
+    const id = e.target.value;
+    setId(id);
+  };
+
+  const login = () => {
+    props.login({ name: name, id: id });
+    props.history.push("/");
+  };
+
+  return (
+    <div>
+      <Form>
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={name}
+            onChange={onChangeName}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>ID</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter id"
+            value={id}
+            onChange={onChangeId}
+          ></Form.Control>
+        </Form.Group>
+        <Button variant="primary" onClick={login}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
